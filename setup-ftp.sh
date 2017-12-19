4d03f92b929559d2c2f95527d7efe0b706b41c06
Gandosha <Gandosha@users.noreply.github.com>
2017-12-19 10:20:28 -0500

    Update setup-ftp.sh

--- setup-ftp.sh
+++ setup-ftp.sh
@@ -2,7 +2,7 @@
 
 groupadd ftpgroup
 useradd -g ftpgroup -d /dev/null -s /etc ftpuser
-pure-pw useradd offsec -u ftpuser -d /ftphome
+pure-pw useradd <user> -u ftpuser -d /ftphome
 pure-pw mkdb
 cd /etc/pure-ftpd/auth
 ln -s ../conf/pureDB 60pdb
