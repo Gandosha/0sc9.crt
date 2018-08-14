6d61aa9ad7a67815abf4346c7def03b5fc92bc08
Gandosha <Gandosha@users.noreply.github.com>
2018-08-14 21:33:41 +0300

    Update HaGashas.go

--- HaGashas.go
+++ HaGashas.go
@@ -9,7 +9,7 @@ import "flag"
 import "os"
 
 /* This function extracts attaker's IP address from ifconfig command output according to the interface that is given as a flag. */
-func whatIsMyIP(netInterface string) {
+func whatIsMyIP(netInterface string) string{
 	ifconfigCmd := exec.Command("ifconfig")
 	ifconfigIn, _ := ifconfigCmd.StdinPipe()
 	ifconfigOut, _ := ifconfigCmd.StdoutPipe()
@@ -25,41 +25,53 @@ func whatIsMyIP(netInterface string) {
 	ifconfigTrimmed2 := ifconfigTrimmed[inetIndex+5:]
 	spaceIndex := strings.Index(ifconfigTrimmed2, " ")
 	ipAddress := ifconfigTrimmed2[:spaceIndex]	
-	fmt.Println(ipAddress)
+	return string(ipAddress)
 }
 
 
-/* This function identifies targets in my current subnet. 
-func identifyTargetsInSubnet */
+/* This function identifies targets in attacker's current subnet and performs a nmap vulnerability scan against them. */
+func scanTargetsInSubnet(myIpAddress string) {
+	//Put myIpAddress in string and then trim it from the end until the first dot
+	s := myIpAddress	
+	fmt.Println(s)
+}
 	
 
 
 func main() {	
-	interfacePtr := flag.String("interface", "nil", "Name of the interface to use (Run ifconfig before HaGashash in order to choose one).")
+	interfacePtr := flag.String("interface", "nil", "Name of the interface to use (Required! Run ifconfig before HaGashash in order to choose one).")
+	var myIpAddress string = whatIsMyIP(*interfacePtr) 
+	fmt.Println(myIpAddress)
 	hostPtr := flag.String("host", "nil", "Skip host discovery. Scan only this host (Type its IP address or domain name).")
-	subnetPtr := flag.String(&ipAddress, "none", ipAddress, "Discover alive hosts in subnet and scan them. (Default - scans /24).")
-	/*dnsPtr := flag.Bool("fork", false, "Locate non-contiguous IP space and hostnames against specified domain. (Type "true" or "false").")
-	/*nmap spoof
+	subnetPtr := flag.Bool(&myIpAddress, "subnet", true, "Discover alive hosts in subnet and scan them.")
+	/*dnsPtr := flag.Bool("dns", false, "Locate non-contiguous IP space and hostnames against specified domain. (Type "true" or "false").")
+	nmap spoof
 	nmap decoy*/
 	flag.Parse()	
 	//whatIsMyIP(*interfacePtr)
 	//fmt.Println(interfacePtr)
 	switch {
 	case *interfacePtr == "nil":
-		fmt.Println("\nPlease specify an interface name. (Ex. -interface=lo)\n\n")	
+		fmt.Println("\n[!] Please specify an interface name. (Ex. -interface=lo)\n\n")	
 		flag.PrintDefaults()
 		fmt.Println("\n")
 		os.Exit(1)
 	case *hostPtr == "nil":
-		fmt.Println("\nPlease specify a target. (Ex. -host=example.com or -host=127.0.0.1)\n\n")	
+		//start to scan subnet
+		fmt.Println("\n[!] Starting to scan your subnet (/24).\n\n")
+		//whatIsMyIP(*interfacePtr)
+		scanTargetsInSubnet(myIpAddress)
+	/*case *dnsPtr == true:
+		//start fierce
+	default:
+		fmt.Println("\n[!] Not enough flags in order to start the program. EXITING!\n\n")	
 		flag.PrintDefaults()
 		fmt.Println("\n")
-		os.Exit(1)
-	case *subnetPtr == "nil":
-		fmt.Println("\nPlease specify a target. (Ex. -host=example.com or -host=127.0.0.1)\n\n")	
-		flag.PrintDefaults()
-		fmt.Println("\n")
-		os.Exit(1)
+		os.Exit(1) */	
 	}
+	/*start to scan subnet
+	fmt.Println("\n[!] Starting to scan your subnet (/24).\n\n")
+	whatIsMyIP(*interfacePtr)
+	scanTargetsInSubnet(ipAddress) */	
 	
 }
