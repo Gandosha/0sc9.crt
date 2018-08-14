5417a11213b7a6c197f9402bc39043d0fb519771
Gandosha <Gandosha@users.noreply.github.com>
2018-08-14 18:58:09 +0300

    Update HaGashas.go

--- HaGashas.go
+++ HaGashas.go
@@ -1,15 +1,12 @@
-/* This progtam is ment for aiding penetration testers in the enumeration proccess. */
+/* This command-line program is ment for aiding penetration testers in the enumeration proccess. */
 package main
 
 import "fmt"
 import "io/ioutil"
 import "os/exec"
 import "strings"
-
-/* This function responsible on program's flags. */
-func flags()
-
-
+import "flag"
+import "os"
 
 /* This function extracts attaker's IP address from ifconfig command output according to the interface that is given as a flag. */
 func whatIsMyIP(netInterface string) {
@@ -32,13 +29,37 @@ func whatIsMyIP(netInterface string) {
 }
 
 
-
 /* This function identifies targets in my current subnet. 
 func identifyTargetsInSubnet */
 	
 
 
-func main() {
-	whatIsMyIP("enp0s3")
+func main() {	
+	interfacePtr := flag.String("interface", "nil", "Name of the interface to use (Run ifconfig before HaGashash in order to choose one).")
+	hostPtr := flag.String("host", "nil", "Skip host discovery. Scan only this host (Type its IP address or domain name).")
+	subnetPtr := flag.String(&ipAddress, "none", ipAddress, "Discover alive hosts in subnet and scan them. (Default - scans /24).")
+	/*dnsPtr := flag.Bool("fork", false, "Locate non-contiguous IP space and hostnames against specified domain. (Type "true" or "false").")
+	/*nmap spoof
+	nmap decoy*/
+	flag.Parse()	
+	//whatIsMyIP(*interfacePtr)
+	//fmt.Println(interfacePtr)
+	switch {
+	case *interfacePtr == "nil":
+		fmt.Println("\nPlease specify an interface name. (Ex. -interface=lo)\n\n")	
+		flag.PrintDefaults()
+		fmt.Println("\n")
+		os.Exit(1)
+	case *hostPtr == "nil":
+		fmt.Println("\nPlease specify a target. (Ex. -host=example.com or -host=127.0.0.1)\n\n")	
+		flag.PrintDefaults()
+		fmt.Println("\n")
+		os.Exit(1)
+	case *subnetPtr == "nil":
+		fmt.Println("\nPlease specify a target. (Ex. -host=example.com or -host=127.0.0.1)\n\n")	
+		flag.PrintDefaults()
+		fmt.Println("\n")
+		os.Exit(1)
+	}
 	
 }
